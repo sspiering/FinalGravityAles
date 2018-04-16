82bb196bd51ea3f8c83ec2beb2c837ec12286208
Shelby Spiering <sspiering@eagles.ewu.edu>
2018-04-15 19:45:53 -0700

    Video BG

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/zitrusfrisch/pen/vymGI.
+
+ with color and dot-grid overlay effect (mimics background-size: cover)
--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var windowH =  $(window).height();
-    $('#main_container, #overlay').height(windowH);
-    $(window).resize(function () {
-        var windowH =  $(window).height();
-        $('#main_container, #overlay').height(windowH);
-    });
-});​
